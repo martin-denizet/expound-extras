22fe22999cfb8e731bfaa758de887c070f71ad4c
Martin DENIZET <martin.denizet@supinfo.com>
2014-01-08 21:17:52 -0800

    First commit. Adds widget area for the header and the footer.

---
+++ README.md
@@ -0,0 +1,25 @@
+# WordPress Expound Extras Theme
+
+Child theme for Konstantin Kovshenin's Expound WordPress theme. The first reason for me to create this theme was to add widget areas.
+
+If you like my work, you're welcome to endorse me!
+[![endorse](https://api.coderwall.com/martin-denizet/endorsecount.png)](https://coderwall.com/martin-denizet)
+You can also visit my blog at http://martin-denizet.com
+
+Work sponsored by Algen, visit us at http://algen.co
+
+## Added Features
+
+* Header widget area
+* Footer widget area
+
+Wish for more? You're welcome to create a feature request or to submit pull request.
+
+## Credits
+
+This is a child theme of the excellent Expound theme by Konstantin Kovshenin: http://kovshenin.com
+Expound is based on Underscores http://underscores.me/ , (C) 2012-2013 Automattic, Inc
+
+## License
+
+GPLv2
---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Register widgetized area and update sidebar with default widgets
+ */
+function expound_extra_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Sidebar', 'expound' ),
+		'id'            => 'sidebar-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+        
+        register_sidebar( array(
+		'name' => __( 'Footer', 'expound' ),
+		'id' => 'sidebar-footer',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'Header', 'expound' ),
+		'id' => 'sidebar-header',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+}
+add_action( 'widgets_init', 'expound_extra_widgets_init' );
+?>

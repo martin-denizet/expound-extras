20208152881fe1fbdd8d701e1aa5fab87781c119
Martin Denizet <martin.denizet@supinfo.com>
2014-08-11 10:01:48 +0800

    Update functions.php for 4 pre-footer columns
    Untested

--- functions.php
+++ functions.php
@@ -30,6 +30,42 @@ function expound_extra_widgets_init() {
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	) );
+	
+	register_sidebar( array(
+		'name' => __( 'Footer Col1', 'expound' ),
+		'id' => 'pre-footer-widgets1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	
+	register_sidebar( array(
+		'name' => __( 'Footer Col2', 'expound' ),
+		'id' => 'pre-footer-widgets2',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	
+	register_sidebar( array(
+		'name' => __( 'Footer Col3', 'expound' ),
+		'id' => 'pre-footer-widgets3',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	
+	register_sidebar( array(
+		'name' => __( 'Footer Col4', 'expound' ),
+		'id' => 'pre-footer-widgets4',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
 }
 add_action( 'widgets_init', 'expound_extra_widgets_init' );
 ?>

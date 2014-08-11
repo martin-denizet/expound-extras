f562470f754ab21622b5d0fb7aa2efef3961f165
Martin Denizet <martin.denizet@supinfo.com>
2014-08-11 10:01:05 +0800

    Update footer.php for 4 pre-footer columns
    Untested

--- footer.php
+++ footer.php
@@ -9,7 +9,12 @@
 ?>
 
 	</div><!-- #main -->
-
+	<div id="pre-footer" class="pre-footer-content">
+    		<div id="pre-footer-widgets1" class="pre-footer-col"><?php dynamic_sidebar( 'pre-footer-widgets1' ) ?></div>
+    		<div id="pre-footer-widgets2" class="pre-footer-col"><?php dynamic_sidebar( 'pre-footer-widgets2' ) ?></div>
+    		<div id="pre-footer-widgets3" class="pre-footer-col"><?php dynamic_sidebar( 'pre-footer-widgets3' ) ?></div>
+    		<div id="pre-footer-widgets4" class="pre-footer-col"><?php dynamic_sidebar( 'pre-footer-widgets4' ) ?></div>
+  	</div>
 	<footer id="colophon" class="site-footer" role="contentinfo">
                 <div id="footer-widgets" class="full-container">
                         <?php dynamic_sidebar( 'sidebar-footer' ) ?>
